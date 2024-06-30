58febff2edf3a1db5a76af85c9b14ad084017fb0
Valentin Delaye <jonesbusy@gmail.com>
2024-06-30 11:11:00 +0200

    Extract maven rewrite plugin to properties from pom

--- plugin-modernizer-core/src/main/java/io/jenkins/tools/pluginmodernizer/core/config/Settings.java
+++ plugin-modernizer-core/src/main/java/io/jenkins/tools/pluginmodernizer/core/config/Settings.java
@@ -1,15 +1,23 @@
 package io.jenkins.tools.pluginmodernizer.core.config;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Settings {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Settings.class);
 
     public static final Path DEFAULT_CACHE_PATH;
 
     public static final Path DEFAULT_MAVEN_HOME;
 
-    public static final String MAVEN_REWRITE_PLUGIN_VERSION = "5.34.1";
+    public static final String MAVEN_REWRITE_PLUGIN_VERSION;
 
     public static final String RECIPE_DATA_YAML_PATH = "recipe_data.yaml";
 
@@ -26,6 +34,7 @@ public class Settings {
             DEFAULT_CACHE_PATH = Paths.get(cacheDirFromEnv);
         }
         DEFAULT_MAVEN_HOME = getDefaultMavenHome();
+        MAVEN_REWRITE_PLUGIN_VERSION = getRewritePluginVersion();
     }
 
     private static Path getDefaultMavenHome() {
@@ -37,5 +46,32 @@ public class Settings {
             return null;
         }
         return Path.of(mavenHome);
+    }
+
+    private static String getRewritePluginVersion() {
+        return readProperty("openrewrite.maven.plugin.version", "versions.properties");
+    }
+
+    private static String readProperty(String key, String resource) {
+        Properties properties = new Properties();
+        try (InputStream input = Settings.class.getClassLoader().getResourceAsStream(resource)) {
+            if (input == null) {
+                LOG.error("Error reading {} from settings", resource);
+                throw new IOException(String.format("Unable to to load `%s`", resource));
+            }
+            properties.load(input);
+        }
+        catch (IOException e) {
+            LOG.error("Error reading from settings", e);
+            return null;
+        }
+
+        String version = properties.getProperty("openrewrite.maven.plugin.version");
+        if (version == null || version.isEmpty()) {
+            LOG.error(String.format("Unable to read `%s` from `%s`", key, resource));
+            return null;
+        }
+
+        return version.trim();
     }
 }
